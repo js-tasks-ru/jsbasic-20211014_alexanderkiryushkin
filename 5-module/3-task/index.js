a4fb88ae0e9cec02d5c0c670a89341abda7bf2e1
Alexander <alexanderkiryushkin@yandex.ru>
2021-11-07 20:38:47 +0300

    module-5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel__inner');
+  const rightButton = document.querySelector('.carousel__arrow_right');
+  const leftButton = document.querySelector('.carousel__arrow_left');
+  const width = carousel.offsetWidth;
+  leftButton.style.display = 'none';
+  let count = 0;
+
+
+  rightButton.addEventListener('click', () => {
+    count++;
+    carousel.style.transform = `translateX(-${width * count}px)`;
+    if (count === 3) {
+      rightButton.style.display = 'none';
+    } else {
+      rightButton.style.display = '';
+      leftButton.style.display = '';
+    }
+  });
+
+
+  leftButton.addEventListener('click', () => {
+    count--;
+    carousel.style.transform = `translateX(-${width * count}px)`;
+    if (count === 0) {
+      leftButton.style.display = 'none';
+    } else {
+      rightButton.style.display = '';
+      leftButton.style.display = '';
+    }
+  });
 }
+
+
