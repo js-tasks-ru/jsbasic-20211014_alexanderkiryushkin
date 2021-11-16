9d4d47c26b1ba302eb5381aada03ba09bc1a0e9a
Alexander <alexanderkiryushkin@yandex.ru>
2021-11-16 22:05:00 +0300

    '6-module'

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,46 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this._rows = rows;
+    this._elem = document.createElement('table');
+    this.render();
+
+  }
+  render() {
+    this._tbody = this._rows.map((item) =>
+      `<tr>
+            <td>${ item.name }</td>
+            <td>${ item.age }</td>
+            <td>${ item.salary }</td>
+            <td>${ item.city }</td>
+            <td><button>X</button></td>
+        </tr>`);
+    this.elem.innerHTML = `
+        <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        ${this._tbody.join('')}
+      </tbody>`;
+
+    const buttons = this._elem.querySelectorAll('button');
+    for (let button of buttons) {
+      button.addEventListener('click', this.onButtonClick);
+    }
+  }
+
+  onButtonClick(event) {
+    const buttonDeleted = event.target.closest('tr');
+    buttonDeleted.remove();
+  }
+
+  get elem() {
+    return this._elem;
   }
 }
